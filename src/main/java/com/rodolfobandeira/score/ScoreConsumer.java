501f7cc729f0691ce598a62a184e7a846bad7ec2
rodolfobandeira <contato@rodolfobandeira.com>
2021-07-01 11:24:57 -0400

    First commit with simple consumer example

---
+++ src/main/java/com/rodolfobandeira/score/ScoreConsumer.java
@@ -0,0 +1,51 @@
+package com.rodolfobandeira.score;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ScoreConsumer {
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Collections.singletonList("NEW_SCORE"));
+
+        while(true) {
+            var records = consumer.poll(Duration.ofMillis(5000));
+
+            if (records.isEmpty()) {
+                System.out.println("No records found");
+            } else {
+
+                for (var record : records) {
+                    System.out.println("Processing new score");
+                    System.out.println("Key: " + record.key());
+                    System.out.println("Value: " + record.value());
+                    System.out.println("Partition: " + record.partition());
+                    System.out.println("Offset: " + record.offset());
+
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Score update processed");
+                }
+            }
+        }
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, ScoreConsumer.class.getSimpleName());
+
+        return properties;
+    }
+}
